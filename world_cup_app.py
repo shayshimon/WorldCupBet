60f79bada547f39f0e39d52a0d7312469d1beaf3
Shay <shay@nutrinohealth.com>
2018-06-20 01:21:43 +0300

    house tables and some ui fixes

--- world_cup_app.py
+++ world_cup_app.py
@@ -8,7 +8,11 @@ def get_standings():
     res = requests.get("http://worldcup.sfg.io/teams/results")
     sorted_res = sorted(res.json(), key=lambda x: (x['group_letter'], x['points'], x['goal_differential'], x['country']), reverse=True)
     stand_groups = groupby(sorted_res, key=lambda x: x['group_letter'])
-    return {i: [next(g)['country'], next(g)['country']] for i, g in stand_groups}
+    house_dict = {}
+    for i, g in stand_groups:
+        house_dict[i] = sorted(list(g), key=lambda x: (x['points'], x['goal_differential'], x['country']), reverse=True)
+
+    return house_dict
 
 app = Flask(__name__)
 
@@ -23,14 +27,14 @@ def present_scores():
         item_rank = 0
         for house in stand_dict:
             house_rank = 0
-            if stand_dict[house][0] == item[house][0]:
+            if stand_dict[house][0]['country'] == item[house][0]:
                 house_rank += 3
-            elif item[house][0] in stand_dict[house]:
+            elif item[house][0] == stand_dict[house][1]['country']:
                 house_rank += 2
 
             if stand_dict[house][1] == item[house][1]:
                 house_rank += 3
-            elif item[house][1] in stand_dict[house]:
+            elif item[house][1] == stand_dict[house][1]['country']:
                 house_rank += 2
 
             item_rank += house_rank
@@ -39,7 +43,7 @@ def present_scores():
 
     sorted_bet_dict = sorted(bet_dict, key=lambda x: x['rank'], reverse=True)
     abc_list = [chr(i) for i in range(ord('A'), ord('I'))]
-    return render_template("wc_rank.html", result=sorted_bet_dict, abc_list=abc_list)
+    return render_template("wc_rank.html", result=sorted_bet_dict, abc_list=abc_list, stand_dict=stand_dict)
 
 @app.route("/end_point")
 def endp():
