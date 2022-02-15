fa9ad2ea4f93854d5d5d09c3deb1a2dfb0b32c4c
Shay <shay@nutrinohealth.com>
2022-02-15 13:22:44 +0200

    added source csv and fix script

--- bet_poll_format.py
+++ bet_poll_format.py
@@ -5,8 +5,8 @@ df = pd.read_csv('./world_cup_bet_poll.csv')
 
 out_json = []
 for ind, row in df.iterrows():
-    tmp_dic = {'name': row[2]}
-    tmp_dic.update({chr(i): [row[(i - ord('A')) * 2 + 3], row[(i - ord('A')) * 2 + 4]] for i in range(ord('A'), ord('I'))})
+    tmp_dic = {'name': row[1]}
+    tmp_dic.update({chr(i): [row[(i - ord('A')) * 2 + 2], row[(i - ord('A')) * 2 + 3]] for i in range(ord('A'), ord('I'))})
     out_json.append(tmp_dic)
 
 with open('bet_poll.json', 'w') as outfile:
